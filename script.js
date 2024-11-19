375b980675bedfb9df30e153897d2c858708894c
ufukozsoy <ufuk.ozsoy@powercoders.org>
2024-11-19 12:16:26 +0100

    updated

--- script.js
+++ script.js
@@ -11,3 +11,8 @@ document.querySelector(".back-to-top").addEventListener("click", function() {
     window.scrollTo({top: 0, behavior: "smooth"});
 });
 
+AOS.init({
+    duration: 1000,  // Animation duration in milliseconds
+    easing: 'ease-in-out',  // Animation easing function
+    once: true  
+  });
